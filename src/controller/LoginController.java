0b7774ed338afc16a93ab27730ee8a6a4898a520
Leonardo Teles <leo_steles@hotmail.com>
2023-08-28 15:58:47 -0300

    Criando controller e model de login

---
+++ src/controller/LoginController.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import view.LoginView;
+
+/**
+ *
+ * @author leona
+ */
+public class LoginController {
+    private LoginView view;
+
+    public LoginController(LoginView view) {
+        this.view = view;
+    }
+}
